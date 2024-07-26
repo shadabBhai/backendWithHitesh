1f5397c98ab081fcb95705abc271d929bb17df9b
Shadab khan <119776858+shadabBhai@users.noreply.github.com>
2024-07-27 02:12:38 +0530

    get user details from frontEnd

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,16 +1,36 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+
 
 const registerUser = asyncHandler(async(req,res)=>{
-      try{
-        res.status(200).json({
-            message:"ok",
-        }) 
-      }catch(err){
-        console.log(err)
-        res.status(500).json({
-            message :'An error  occurred '
-        })
+//get user details from  frontend
+//validation - not empty
+//check if user already exists : username , email
+// check for images , check for avatar
+//upload them to cloudinary , avatar
+// create user object - create entry in  db 
+// remove  password and  refresh token field from response
+// check  for user creation 
+//return  res
+
+      {
+       const {fullName  , email , userName , password}  = req.body
+       if (
+            [fullName , email , userName , password].some((field)=>
+            field?.trim() === "")
+        ) {
+        throw new ApiError(400 , "All filed are  required")
+       }
+
+
       }
+
+      // }catch(err){
+      //   console.log(err)
+      //   res.status(500).json({
+      //       message :'An error  occurred '
+      //   })
+      // }
 })
 
 export {registerUser}
