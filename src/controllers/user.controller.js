3384bdfbb35e98bd9146de9e563cf8bcd608239f
Shadab khan <119776858+shadabBhai@users.noreply.github.com>
2024-07-28 02:55:20 +0530

    auth middleware , adding login and logout funtionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -105,7 +105,7 @@ const loginUser = asyncHandler(async(req,res)=>{
      //send cookie
      const {email ,userName , password} = req.body
 
-     if(!userName || ! email){
+     if(!(userName ||  email)){
           throw new ApiError(400, "username or email is required")
      }
      const user = await User.findOne({
@@ -120,7 +120,51 @@ const loginUser = asyncHandler(async(req,res)=>{
           throw new ApiError(401 , "Passsword is not correct")
      }
     const {accessToken , refreshToken}= await generateAccessAndRefreshTokens(user._id)
-      const loggedInUser = await User.findById(user._id).select("-password  -refreshToken")
+
+    const loggedInUser = await User.findById(user._id).select("-password  -refreshToken")
+
+    const options ={
+     httpOnly : true,
+     secure:true
+    }
+
+    return  res.status(200)
+    .cookie("accessToken" , accessToken ,options)
+    .cookie("refreshToken " , refreshToken , options)
+    .json(
+     new ApiResponse(200,{
+          user : loggedInUser ,
+          accessToken,
+          refreshToken
+     },"User logged In Sucessfully")
+    )
 
 })
-export {registerUser , loginUser}
+
+const logoutUser = asyncHandler(async(req,res)=>{
+     await User.findByIdAndUpdate(
+          req.user._id,
+          {
+               $set:{
+                    refreshToken: undefined
+               }
+          },{
+               new: true
+          }
+     )
+
+     const options ={
+          httpOnly : true,
+          secure:true
+         }
+
+     return res
+     .status(200)
+     .clearCookie("accessToken" , options)
+     .clearCookie("refreshToken" , options)
+     .json(new ApiResponse(200, {} , "User logged Out"))
+
+
+
+})
+export {registerUser , loginUser ,logoutUser}
